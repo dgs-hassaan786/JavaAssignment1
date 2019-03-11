fc526b18cacc15e1f75d2e4c5050d4fb4aa7b3f5
hassaan <hassaankhan786@gmail.com>
2019-03-11 00:28:46 +0000

    Java Bus Simulator implementation
    Signed-off-by: hassaan <hassaankhan786@gmail.com>

---
+++ CPPAssignment1/src/io/bus/syncer/Passenger.java
@@ -0,0 +1,83 @@
+package io.bus.syncer;
+
+import java.util.Date;
+/**
+ * It contains the definition for the information holding related the Passenger. The class is a thread-safe.
+ */
+public class Passenger {	
+
+	/**
+	 * Arrival DateTime of the passenger
+	 */
+	Date arrivalDateTime;
+	/**
+	 * OnBoard DateTime of the passenger
+	 */
+	Date onBoardDateTime;
+	/**
+	 * Departure DateTime of the passenger 
+	 */
+	Date departureDateTime;
+	/**
+	 * OnBoarding Station number
+	 */
+	int onBoardStation;
+	/**
+	 * Departure Station number
+	 */
+	int departureStation;
+
+	/**
+	 * The constructor for the Passenger class 
+	 * @param obSt - The onBoarding Station number
+	 * @param dpSt - departure Station number
+	 */
+	public Passenger(int obSt, int dpSt){
+		onBoardStation = obSt;
+		departureStation = dpSt;
+		arrivalDateTime = new Date();
+	}
+
+	/**
+	 * setter property for onBoardDateTime
+	 */
+	public synchronized void setOnBoardDate(){
+		onBoardDateTime = new Date();
+	}
+
+	/**
+	 * setter property for departureDateTime
+	 */
+	public synchronized void setDepartureDate(){
+		departureDateTime = new Date();
+	}
+
+	/**
+	 * getter property for departureDateTime
+	 */
+	public synchronized int getDepartureStation(){
+		return departureStation;
+	}
+
+
+	/**
+	 * overrides the default functionality of Passenger class
+	 * @return sample string - "Passenger arrived at station no: 1 at datetime and onboard on bus at datetime and departure at datetime at station no: 4"
+	 */
+	@Override
+	public synchronized String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Passenger arrived at station no: ");
+		builder.append(onBoardStation);
+		builder.append(" at ");
+		builder.append(arrivalDateTime);
+		builder.append(" and onboarded on bus at: ");
+		builder.append(onBoardDateTime);
+		builder.append(" and departure at: ");
+		builder.append(departureDateTime);
+		builder.append(" at station no: ");
+		builder.append(departureStation);		
+		return builder.toString();
+	}
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# JavaAssignment1
+# Java Assignment 1
+
+# Write a program to simulate a bus traveling between 5 different stations and repeats the cycle, the bus can take up to a maximum of 50 persons, at each station random number of persons get off the bus and random number of persons get on the bus, consider these cases
+
+- If bus does not have enough space for all persons, persons will have to
+stay in station for next cycle
+- Persons cannot mount on bus until persons on bus dismount first.
+- You can simulate bus trip with a fixed delay between each stop to
+simulate travel time.
+- Persons can not mount/dismount the bus until bus arrives to the
+designated station.
+
+# Hints:
+Use semaphores to control access to the bus and other utilities to control access
+to bus. Use thread pools to manage thread management. You can use latches,
+cyclic barriers, re-entrant locks and condition variables to write your code.
